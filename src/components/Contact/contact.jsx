b5dd24905ee409f1f8785e974c8e870951177d6f
YASHSAXEN <yashsaxena210403@gmail.com>
2024-03-22 12:57:43 +0530

    add mail and notification functionality in contact form

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,7 +1,36 @@
 import styles from './contact.module.css'
 import { motion } from 'framer-motion'
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function Contact(){
+    let form = useRef()
+    let userName = useRef()
+    let userEmail = useRef()
+    let userSubject = useRef()
+    let userMessage = useRef("");
+
+    let sendEmail = (e) =>{
+
+        e.preventDefault()
+
+        emailjs.sendForm('service_71wwr1o', 'template_i0xq5sh', form.current, {publicKey: 'Z4c8NnWiz5-1oDwbZ'})
+        .then(
+          () => {
+            userName.current.value =''
+            userEmail.current.value =''
+            userSubject.current.value =''
+            userMessage.current.value =''
+            toast.success("Successfully Submitted, I will reach you soon");
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+
+    }
 
     let parentVariant = {
         initial:{
@@ -29,20 +58,23 @@ function Contact(){
     }
 
     return(
-        <div className={styles.contactContainer}>
-            <motion.div className={styles.contactMain} variants={parentVariant} initial="initial" animate="animate">
-                <motion.div className={styles.contactHeader} variants={childrenVariant}>
-                    <p>Contact</p>
+        <>
+            <div className={styles.contactContainer}>
+                <motion.div className={styles.contactMain} variants={parentVariant} initial="initial" animate="animate">
+                    <motion.div className={styles.contactHeader} variants={childrenVariant}>
+                        <p>Contact</p>
+                    </motion.div>
+                    <motion.form className={styles.inputContainer} variants={parentVariant} ref={form} onSubmit={(e)=>{sendEmail(e)}}>
+                        <motion.input type="email" placeholder='Your Email' variants={childrenVariant} name='userEmail' ref={userEmail}/>
+                        <motion.input type="text" placeholder='Your Name'  variants={childrenVariant} name='userName' ref={userName}/>
+                        <motion.input type="text" placeholder='Subject' variants={childrenVariant} name='userSubject' ref={userSubject}/>
+                        <motion.textarea cols="30" rows="10" placeholder='Message' variants={childrenVariant} name='userMessage' ref={userMessage}></motion.textarea>
+                        <motion.button variants={childrenVariant}>Send</motion.button>
+                    </motion.form>
                 </motion.div>
-                <motion.div className={styles.inputContainer} variants={parentVariant}>
-                    <motion.input type="email" placeholder='Your Email' variants={childrenVariant}/>
-                    <motion.input type="text" placeholder='Your Name'  variants={childrenVariant}/>
-                    <motion.input type="text" placeholder='Subject' variants={childrenVariant}/>
-                    <motion.textarea cols="30" rows="10" placeholder='Message' variants={childrenVariant}></motion.textarea>
-                    <motion.button variants={childrenVariant}>Send</motion.button>
-                </motion.div>
-            </motion.div>
-        </div>
+                <ToastContainer />
+            </div>
+      </>
     )
 }
 
